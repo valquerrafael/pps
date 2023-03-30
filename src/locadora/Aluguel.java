aaf9033ccf4ffd2a2309f70e5a9008f5f44de16f
Valquer Rafael <valquer.rafael@gmail.com>
2023-03-30 20:38:49 -0300

    lista 01.2.3

--- src/locadora/Aluguel.java
+++ src/locadora/Aluguel.java
@@ -16,4 +16,38 @@ public class Aluguel {
     public int getDiasAlugado() {
         return diasAlugado;
     }
+
+    public double getValorDeAluguel() {
+        double valor = 0;
+
+        switch (this.dvd.getCodigoDePreco()) {
+            case DVD.NORMAL -> {
+                valor += 2.0;
+                if (this.diasAlugado > 2) {
+                    valor += (this.diasAlugado - 2) * 1.5;
+                }
+            }
+            case DVD.LANCAMENTO -> valor += this.diasAlugado * 3.00;
+            case DVD.INFANTIL -> {
+                valor += 1.5;
+                if (this.diasAlugado > 3) {
+                    valor += (this.diasAlugado - 3) * 1.5;
+                }
+            }
+        }
+
+        return valor;
+    }
+
+    public int getPontosDeAlugadorFrequente() {
+        int pontosDeAlugadorFrequente = 1;
+
+        if (
+            this.dvd.getCodigoDePreco() == DVD.LANCAMENTO &&
+            this.diasAlugado > 1
+        )
+            pontosDeAlugadorFrequente++;
+
+        return pontosDeAlugadorFrequente;
+    }
 }
--- src/locadora/Cliente.java
+++ src/locadora/Cliente.java
@@ -22,64 +22,25 @@ public class Cliente {
 
     public String extrato() {
         final String fimDeLinha = System.getProperty("line.separator");
-        double valorTotal = 0.0;
-        int pontosDeAlugadorFrequente = 0;
 
-        Iterator<Aluguel> alugueis = dvdsAlugados.iterator();
-        String resultado = "Registro de Alugueis de " + getNome() + fimDeLinha;
+        Iterator<Aluguel> alugueis = this.dvdsAlugados.iterator();
+        String extrato = "Registro de Alugueis de " + getNome() + fimDeLinha;
 
         while(alugueis.hasNext()) {
             Aluguel aluguel = alugueis.next();
-            double valorCorrente = this.valorDeAluguel(aluguel);
-            pontosDeAlugadorFrequente += this.pontosDeAlugadorFrequente(aluguel);
 
-            resultado += this.extratoAluguel(aluguel);
-            valorTotal += valorCorrente;
+            extrato += this.extratoAluguel(aluguel);
         }
 
-        resultado += this.extratoTotal(valorTotal, pontosDeAlugadorFrequente);
+        extrato += this.extratoTotal(this.getValorTotal(), this.getPontosTotaisDeAlugadorFrequente());
 
-        return resultado;
-    }
-
-    private double valorDeAluguel(Aluguel aluguel) {
-        double valor = 0;
-
-        switch (aluguel.getDVD().getCodigoDePreco()) {
-            case DVD.NORMAL -> {
-                valor += 2.0;
-                if (aluguel.getDiasAlugado() > 2) {
-                    valor += (aluguel.getDiasAlugado() - 2) * 1.5;
-                }
-            }
-            case DVD.LANCAMENTO -> valor += aluguel.getDiasAlugado() * 3.00;
-            case DVD.INFANTIL -> {
-                valor += 1.5;
-                if (aluguel.getDiasAlugado() > 3) {
-                    valor += (aluguel.getDiasAlugado() - 3) * 1.5;
-                }
-            }
-        }
-
-        return valor;
-    }
-
-    private int pontosDeAlugadorFrequente(Aluguel aluguel) {
-        int paf = 1;
-
-        if (
-            aluguel.getDVD().getCodigoDePreco() == DVD.LANCAMENTO &&
-            aluguel.getDiasAlugado() > 1
-        )
-            paf++;
-
-        return paf;
+        return extrato;
     }
 
     private String extratoAluguel(Aluguel aluguel) {
         final String fimDeLinha = System.getProperty("line.separator");
 
-        return "\t" + aluguel.getDVD().getTitulo() + "\t R$ " + this.valorDeAluguel(aluguel) + fimDeLinha;
+        return "\t" + aluguel.getDVD().getTitulo() + "\t R$ " + aluguel.getValorDeAluguel() + fimDeLinha;
     }
 
     private String extratoTotal(double valorTotal, int pontosDeAlugadorFrequente) {
@@ -88,5 +49,31 @@ public class Cliente {
         return "Valor total pago: R$ " + valorTotal + fimDeLinha +
                 " Voce acumulou " + pontosDeAlugadorFrequente +
                 " pontos de alugador frequente";
+    }
+
+    private double getValorTotal() {
+        Iterator<Aluguel> alugueis = this.dvdsAlugados.iterator();
+        double valorTotal = 0;
+
+        while (alugueis.hasNext()) {
+            Aluguel aluguel = alugueis.next();
+
+            valorTotal += aluguel.getValorDeAluguel();
+        }
+
+        return valorTotal;
+    }
+
+    private int getPontosTotaisDeAlugadorFrequente() {
+        Iterator<Aluguel> alugueis = this.dvdsAlugados.iterator();
+        int pontosTotaisDeAlugadorFrequente = 0;
+
+        while (alugueis.hasNext()) {
+            Aluguel aluguel = alugueis.next();
+
+            pontosTotaisDeAlugadorFrequente += aluguel.getPontosDeAlugadorFrequente();
+        }
+
+        return pontosTotaisDeAlugadorFrequente;
     }
 }
